7fa43b7decf4c76d7ed953b0f5fc1c5f481e641f
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2014-03-04 18:22:00 +0100

    Step 5

--- app.js
+++ app.js
@@ -8,6 +8,11 @@
 var express = require('express');
 var app = express();
 
+app.configure(function(){
+	app.set('view engine', 'ejs');
+	app.set('views', __dirname+"/views");
+})
+
 app.use(express.static(__dirname+"/public"));
 
 app.get('/hello.txt/:name?', function(req, res){
@@ -15,14 +20,9 @@ app.get('/hello.txt/:name?', function(req, res){
 	res.end("Hello "+name+"!");
 });
 
-app.get('/hello.html', function(req, res){
-	res.end(
-		"<!DOCTYPE html>"
-			+"<html>"
-			+"<head><link rel='stylesheet' href='style.css' /></head>"
-			+"<body><h1>Hello World!</h1></body>"
-			+"</html>"
-	);
+app.get('/hello.html/:name?', function(req, res){
+	var name = req.param('name', "World");
+	res.render('hello.ejs', {name: name});
 });
 
 app.listen(80);
